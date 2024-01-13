91e756990a82bb0c980ead6d50d442db5c1c8b76
Dario8517 <darioavalos8517@gmail.com>
2024-01-12 23:58:53 -0300

    Finalizado app

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -6,6 +6,7 @@ import { Browser } from '@capacitor/browser';
 
 import { File } from '@awesome-cordova-plugins/file/ngx';
 import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root'
@@ -89,63 +90,41 @@ export class DataLocalService {
 
     });
 
-    this.crearArchivoFisico( arrTemp.join('') );
+    this.writeSecretFile( arrTemp.join('') );
   
   }
 
-  crearArchivoFisico( text: string ){
 
-    this.file.checkFile( this.file.externalDataDirectory, 'registros.csv' )
-      .then( existe => {
-        console.log('Existe archivo?', existe);
-        return this.escribirEnArchivo( text );
-      })
-      .catch( err => {
-
-        return this.file.createFile( this.file.externalDataDirectory, 'registros.csv', false)
-          .then( creado => this.escribirEnArchivo( text ) )
-          .catch( err2 => console.log ('No se pudo crear el archivo', err2));
-      })
-
-  }
-
-  async escribirEnArchivo( text: string ) {
-
-    await this.file.writeExistingFile( this.file.externalDataDirectory, 'registros.csv', text);
-
-    const archivo = `${this.file.externalDataDirectory}registro.csv`;
-    // console.log(this.file.dataDirectory + 'registro.csv');
-
+  writeSecretFile = async ( text: string ) => {
+    await Filesystem.writeFile({
+      path: 'registro.txt',
+      data: text,
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+    type contents = string
+    const contents = await Filesystem.readFile({
+      path: 'registro.txt',
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+    console.log('secrets:', contents);
+    
     const email = {
       to: 'darioavalos8517@gmail.com',
       // cc: 'darioavalos8517@gmail.com',
       // bcc: ['',''],
       attachments: [
-        archivo
+        'file:///storage/emulated/0/Documents/registro.txt'
       ],
-      subject: 'Backup de scans',
+      subject: 'Backup del escaneer',
       body: 'Aqui tienen sus backup - ScanApp',
       isHtml: true
     };
-
-    this.emailComposer.open(email);
-  }
-
-  // writeSecretFile = async ( text: string ) => {
-  //   await Filesystem.writeFile({
-  //     path: 'secrets/registro.txt',
-  //     data: 'This is a test',
-  //     directory: Directory.Documents,
-  //     encoding: Encoding.UTF8,
-  //   });
-
-  //   const contents = await Filesystem.readFile({
-  //     path: 'secrets/text.txt',
-  //     directory: Directory.Documents,
-  //     encoding: Encoding.UTF8,
-  //   });
   
-  //   console.log('secrets:', contents);
-  // };
+    this.emailComposer.open(email);
+  };
+
+
 
 }
